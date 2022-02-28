389071a07f75d9d98325d6be50382b8b8846ef62
mohdraheez <42299547+mohdraheez02@users.noreply.github.com>
2022-02-28 15:35:52 +0530

    Update dslab_2.c

--- dslab_2.c
+++ dslab_2.c
@@ -1,28 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 void patternmatch();
+
 char str[40],pat[40],rep[30],ans[30];
 int i=0,s=0,p=0,r=0,a=0,flag=0;
+
 void main()
 {
     printf("enter the main string\n");
     scanf("%[^\n]",str);
+    
     printf("enter the pattern string\n");
     scanf("%*c%[^\n]",pat);
+    
     printf("enter the replacement string\n");
     scanf("%*c%[^\n]",rep);
+    
     patternmatch();
 }
+
 void patternmatch()
 {
     while(str[i]!='\0')
     {
+        
         if(str[s]==pat[p])
         {
             s++;
             p++;
+            
             if(pat[p]=='\0')
             {
+                
                 for(r=0;rep[r]!='\0';r++)
                 {
                     ans[a]=rep[r];
@@ -30,9 +40,11 @@ void patternmatch()
                     i=s;
                     flag=1;
                     p=0;
+                    
                 }
             }
         }
+        
     else
     {
         ans[a]=str[i];
@@ -42,9 +54,10 @@ void patternmatch()
         p=0;
     }
 }
+    
     if(flag==1)
     {
-    printf("answer string is %s\n",ans);
+        printf("answer string is %s\n",ans);
     }
     else
     {
