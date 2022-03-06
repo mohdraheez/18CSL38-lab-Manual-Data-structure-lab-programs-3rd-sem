5116c13c77d712ee270d02e4532a5810d8815f09
mohdraheez <42299547+mohdraheez02@users.noreply.github.com>
2022-03-06 19:35:25 +0530

    Update dslab_5b.c

--- dslab_5b.c
+++ dslab_5b.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
-int count=0;
+
 void tower(int n,char beg,char aux,char end )
 {
     if(n==1)
     {
         printf("move %d disk from %c to %c\n",n,beg,end);
-        count++;
-        printf("count is %d\n",count);
     }
     else
     {
         tower(n-1,beg,end,aux);
         printf("move %d disk from %c to %c\n",n,beg,end);
-        count++;
-        printf("count is %d\n",count);
         tower(n-1,aux,beg,end);
     }
 }
